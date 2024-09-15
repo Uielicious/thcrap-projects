3403087ad639162929d12b4e2c50b2ad43ebc71c
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-15 10:07:48 -0500

    Update th08.v1.00d.js

--- story_mode/th08.v1.00d.js
+++ story_mode/th08.v1.00d.js
@@ -2,11 +2,13 @@
 	"binhacks": {
 		"remove_1cc_requirement_for_Stage_6B": {
 			"addr": "Rx33A79",
-			"code": "EB 16"
+			"code": "EB 16",
+			"expected": "7E 16"
 		},
 		"enable_continues_on_Stage_6B": {
 			"addr": "Rx0496E",
-			"code": "83 3D CCD26401 09"
+			"code": "83 3D CCD26401 09",
+			"expected": "83 3D CCD26401 07"
 		}
 	}
 }
