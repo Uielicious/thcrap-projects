cef00a167805c90e66a2c1b6ac832bd5da1872c4
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-15 11:59:55 -0500

    story mode th06 and th07 compatibility

---
+++ story_mode/th06.v1.02h.js
@@ -0,0 +1,9 @@
+{
+	"binhacks": {
+		"remove_1cc_requirement_for_Good_Ending": {
+			"addr": "Rx10C98",
+			"code": "EB 50",
+			"expected": "74 50"
+		}
+	}
+}
---
+++ story_mode/th07.v1.00b.js
@@ -0,0 +1,9 @@
+{
+	"binhacks": {
+		"remove_1cc_requirement_for_Good_Ending": {
+			"addr": "Rx1E74B",
+			"code": "EB 13",
+			"expected": "74 13"
+		}
+	}
+}
