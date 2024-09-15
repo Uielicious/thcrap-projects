7031a1ea930218ef76c244a48e89e3f09915cab5
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-15 04:31:21 -0500

    storu2
    st

--- story_mode/files.js
+++ story_mode/files.js
@@ -1,11 +1,19 @@
 {
-	"end00a.end": 3106475419,
-	"end00a.end.jdiff": 914700670,
-	"end01a.end": 297310250,
-	"end01a.end.jdiff": 3339855809,
-	"end02a.end": 420293923,
-	"end02a.end.jdiff": 672099336,
-	"end03a.end": 3632899096,
-	"end03a.end.jdiff": 2885798692,
-	"patch.js": 4215415952
+	"end00a.end": null,
+	"end00a.end.jdiff": null,
+	"end01a.end": null,
+	"end01a.end.jdiff": null,
+	"end02a.end": null,
+	"end02a.end.jdiff": null,
+	"end03a.end": null,
+	"end03a.end.jdiff": null,
+	"patch.js": 4215415952,
+	"th08/end00a.end": 3106475419,
+	"th08/end00a.end.jdiff": 914700670,
+	"th08/end01a.end": 297310250,
+	"th08/end01a.end.jdiff": 3339855809,
+	"th08/end02a.end": 420293923,
+	"th08/end02a.end.jdiff": 672099336,
+	"th08/end03a.end": 3632899096,
+	"th08/end03a.end.jdiff": 2885798692
 }
