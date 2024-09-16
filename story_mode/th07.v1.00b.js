0fb5b142ac5d35cb04d2554109d10fcc095c9d36
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-16 02:19:40 -0500

    story_mode th07 Easy Bad Ending
    Altered the ending instructions for th07 to trigger the Bad Ending when clearing on Easy, so that the player can still access them. Previous code left disabled in case want to reverse.

--- story_mode/th07.v1.00b.js
+++ story_mode/th07.v1.00b.js
@@ -1,9 +1,26 @@
 {
 	"binhacks": {
 		"remove_1cc_requirement_for_Good_Ending": {
+			"enable": false,
 			"addr": "Rx1E74B",
 			"code": "EB 13",
 			"expected": "74 13"
+		},
+		"change_1cc_requirement_to_Normal+_Clear": {
+			"enable": true,
+			"addr": "Rx1E73F",
+			"code": "\
+0FB6 05 80626200 \
+909090 \
+85 C0 \
+75 13 \
+",
+			"expected": "\
+8B 15 78626200 \
+0FB6 42 20 \
+85 C0 \
+74 13 \
+"
 		}
 	}
 }
