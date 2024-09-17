00704d621d82b132333902371485e4ca464e6c89
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-17 05:13:43 -0500

    new patch portable_savedata (testing)

---
+++ portable_savedata/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "portable_savedata",
+	"servers": [
+		"https://mirrors.thpatch.net/Uielicious/portable_savedata/",
+		"https://raw.githubusercontent.com/Uielicious/uies-thcrap-projects/master/portable_savedata/"
+	],
+	"title": "(experimental, DO NOT INSTALL) attempting to redirect save data from the AppData folder to the game directory"
+}
---
+++ portable_savedata/th125.v1.00a.js
@@ -0,0 +1,9 @@
+{
+	"binhacks": {
+		"change_ShanghaiAlice_folder": {
+			"addr": "RxA10A9",
+			"code": "55 73 65 72 00 00 00 00 00 00 00 00 00",
+			"expected": "53 68 61 6E 67 68 61 69 41 6C 69 63 65"
+		}
+	}
+}
