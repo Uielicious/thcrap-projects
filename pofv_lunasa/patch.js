d09f62ad549f531410468833be51ad0d014c0782
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-22 11:03:34 -0500

    attempting to create a testing branch

---
+++ pofv_lunasa/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "pofv_lunasa",
+	"servers": [
+		"https://mirrors.thpatch.net/Uielicious/pofv_lunasa/",
+		"https://raw.githubusercontent.com/Uielicious/uies-thcrap-projects/master/pofv_lunasa/"
+	],
+	"title": "(th09) Replaces Lyrica's slot with Lunasa for Story Mode, including a new script."
+}
--- repo.js
+++ repo.js
@@ -4,12 +4,13 @@
 	"patches": {
 		"always_poc": "Enables the Point of Collection line in th06, even when not at Full Power",
 		"eosd_practice_replay": "(th06) Enables saving replays during Practice Mode, like PCB+",
+		"pofv_lunasa": "(th09) Replaces Lyrica's slot with Lunasa for Story Mode, including a new script.",
 		"portable_savedata": "(experimental, DO NOT INSTALL) attempting to redirect save data from the AppData folder to the game directory",
 		"story_mode": "(th06-08,10-18,12.8) Achieve Good Endings on Normal and above, and Bad Endings on Easy, regardless of if you use continues. Convenient for plaything through the games for the story."
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/Uielicious/",
-		"https://raw.githubusercontent.com/Uielicious/uies-thcrap-projects/master/"
+		"https://raw.githubusercontent.com/Uielicious/uies-thcrap-projects/testing/"
 	],
 	"title": "Uielicious Patch Shack"
 }
