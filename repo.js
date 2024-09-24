8c0b6978922dbd13fac2f1f7ed0624b91c17c879
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-09-23 22:55:45 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -4,7 +4,6 @@
 	"patches": {
 		"always_poc": "Enables the Point of Collection line in th06, even when not at Full Power",
 		"eosd_practice_replay": "(th06) Enables saving replays during Practice Mode, like PCB+",
-		"portable_savedata": "(experimental, DO NOT INSTALL) attempting to redirect save data from the AppData folder to the game directory",
 		"story_mode": "(th06-08,10-18,12.8) Achieve Good Endings on Normal and above, and Bad Endings on Easy, regardless of if you use continues. Convenient for plaything through the games for the story."
 	},
 	"servers": [
