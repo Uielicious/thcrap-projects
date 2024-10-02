346a8b131d4ff1a6b5a79b0f2abb1c5f7e6bb641
Uielicious <114707246+Uielicious@users.noreply.github.com>
2024-10-02 14:56:04 -0500

    vocal arrangement kommt

---
+++ eosd_vocal/files.js
@@ -0,0 +1,6 @@
+{
+	"Hier kommt Remilia.wav": 2163297754,
+	"patch.js": 1811055463,
+	"th06/th06_13.loop.wav": 1493735687,
+	"th06/th06_13.wav": 156272394
+}
---
+++ eosd_vocal/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "eosd_vocal",
+	"servers": [
+		"https://raw.githubusercontent.com/Uielicious/thcrap-projects/testing/eosd_vocal/"
+	],
+	"title": "(th06) Replaces Septette for the Dead Princess with a vocal arrangement by けんしん"
+}
